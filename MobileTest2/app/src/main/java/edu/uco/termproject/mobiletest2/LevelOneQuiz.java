cc470516bbc7155639bce2c4e72ce2f7d85993cd
Michael McGregor <mcgregor9033@yahoo.com>
2015-10-25 18:59:42 -0500

    level 1 quiz

---
+++ MobileTest2/app/src/main/java/edu/uco/termproject/mobiletest2/LevelOneQuiz.java
@@ -0,0 +1,64 @@
+package edu.uco.termproject.mobiletest2;
+
+import android.app.Activity;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class LevelOneQuiz extends Activity {
+
+    TextView question;
+    ImageButton box00, box01, box10, box11;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_one_quiz);
+
+        question = (TextView) findViewById(R.id.word);
+        box00 = (ImageButton) findViewById(R.id.button00);
+        box01 = (ImageButton) findViewById(R.id.button01);
+        box10 = (ImageButton) findViewById(R.id.button10);
+        box11 = (ImageButton) findViewById(R.id.button11);
+
+        Resources res = getResources();
+        TypedArray pictures = res.obtainTypedArray(R.array.pictures);
+        Drawable drawable = pictures.getDrawable(0);
+
+        TypedArray names = res.obtainTypedArray(R.array.names);
+        String name = names.getString(0);
+
+        question.setText(name);
+        box00.setImageDrawable(drawable);
+        box01.setImageDrawable(drawable);
+        box10.setImageDrawable(drawable);
+        box11.setImageDrawable(drawable);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_level_one_quiz, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
